69b2137b4cf99fec755d79405cdcc62127d9c079
Kat McCormick <mccormick@uoneuro.uoregon.edu>
2013-04-02 17:36:00 -0700

    Added cycle plotting functionality

--- cycle_avg.m
+++ cycle_avg.m
@@ -5,12 +5,28 @@
 function [avgcycle, norm_cycles] = cycle_avg(theta)
 
 times = upcross2(theta);
-norm_cycles = NaN(length(times)-1, 100);
+norm_cycles = inf(length(times)-1, 500);
 
 for i= 1:length(times)-1
     cyclei = theta(times(i):times(i+1),1);
-    if length(cyclei)>35 %% This is meant to weed out reversals / other blips, but may need adjustment
-        norm_cycles(i,:)= interpft(cyclei,100);
+    if length(cyclei)>55 %% This is meant to weed out reversals / other blips, but may need adjustment
+        norm_cycles(i,:)= interpft(cyclei,500);
     end
 end
     avgcycle = nanmean(norm_cycles);
+    
+    %% plotting
+    list = [];
+    figure;
+    for i = 1:size(norm_cycles,1);
+        if isfinite(norm_cycles(i,1)); % if the first element is not a NaN...
+        list = [list i];  % add the row number to the list
+        end
+    end
+    numb = numel(list);  %how many real cycles are there?
+    m = 3;
+    n = ceil(numb/m);
+    for p = 1:numb
+        subplot(m,n,p);
+        plot(norm_cycles((list(p)),:));
+    end
--- upcross2.m
+++ upcross2.m
@@ -10,7 +10,7 @@ j = 1;
 for i = 2:length(theta)
     signtest = theta(i-1)*theta(i);
     
-    if signtest <= 0 && theta(i) > theta(i-1)  % to get only upward-going mean crossings
+    if signtest <= 0 && theta(i) > theta(i-1)  % to get only upward-going zero crossings
         crosstimes(j) = i;
         j = j+1;
     end
